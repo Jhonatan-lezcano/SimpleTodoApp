b809d74d40db4caa8aeeeb14152df5d24fe7ad1b
jhonatan lezcano <jhonatanlezcano.plv@gmail.com>
2022-08-10 16:29:49 -0500

    :heavy_plus_sign: add dependencies and configuration react-native-screens

--- android/app/src/main/java/com/simpletodoapp/MainActivity.java
+++ android/app/src/main/java/com/simpletodoapp/MainActivity.java
@@ -3,9 +3,14 @@ package com.simpletodoapp;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
-
+  // react-native-screen package configuration
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
